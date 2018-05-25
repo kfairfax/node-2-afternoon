b40b11a1241d0e9a54a0385d4bf66766cbdc3936
Katrina Fairfax <fairfaxkatrina@gmail.com>
2018-05-24 22:14:25 -0600

    project finished

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-massive(process.env.connectionString).then(dbInstance=>{
+massive(process.env.CONNECTION_STRING).then(dbInstance=>{
     app.set('db', dbInstance);
 })
 
-app.post('/api/products', products_controller.create);
-app.get('/api/products:id', products_controller.getOne);
-app.get('/api/products', products_controller.getAll);
-app.put('/api/products:id', products_controller.update);
-app.delete('/api/products:id', products_controller.delete);
+app.post( '/api/product', products_controller.create );
+app.get( '/api/products', products_controller.getAll );
+app.get( '/api/product/:id', products_controller.getOne );
+app.put( '/api/product/:id', products_controller.update );
+app.delete( '/api/product/:id', products_controller.delete );
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{`Listening on port ${port}.`})
